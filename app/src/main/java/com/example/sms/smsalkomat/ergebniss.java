70ff6465258da6d188169358ebcad6e1d425bb28
Mattia Moroder <mattia.moroder@live.it>
2018-06-06 09:12:23 +0200

    erste struktur der basic-activities

---
+++ app/src/main/java/com/example/sms/smsalkomat/ergebniss.java
@@ -0,0 +1,13 @@
+package com.example.sms.smsalkomat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ergebniss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ergebniss);
+    }
+}
